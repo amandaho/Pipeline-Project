b2137a485094296abe43cd013ad418e75859e8cd
Christopher Woods <Christopher.Woods@LibertyMutual.com>
2017-07-18 15:50:29 -0400

    Add top and side navigation

--- Pipeline-Project/src/app/app.module.ts
+++ Pipeline-Project/src/app/app.module.ts
@@ -2,17 +2,21 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
-import { HNavigationComponent } from './h-navigation/h-navigation.component';
 import { HomeComponent } from './home/home.component';
+
+import { MaterialModule } from '@angular/material';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
+import 'hammerjs';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HNavigationComponent,
     HomeComponent,
   ],
   imports: [
     BrowserModule,
+    MaterialModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
