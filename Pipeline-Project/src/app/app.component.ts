175527374c97a97e3b5d52494cb8b07dfb574bb1
Amanda Ho <amanda.ho@libertymutual.com>
2017-07-18 15:53:17 -0400

    Added new car icons

--- Pipeline-Project/src/app/app.component.ts
+++ Pipeline-Project/src/app/app.component.ts
@@ -1,26 +1,31 @@
 import { Component, OnInit } from '@angular/core';
 import { HiveService } from './hive.service'
 
+import {Directive, EventEmitter, OnChanges, OnDestroy, SimpleChange,
+  AfterContentInit, ContentChildren, QueryList, Input, Output
+} from '@angular/core';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
+
 export class AppComponent implements OnInit{
   
   markers: any;
 
-  constructor(private HiveService: HiveService){}
+  constructor(private HiveService: HiveService){
+  }
 
   getLocation(){
     this.HiveService.getRecords()
       .subscribe(
           markers => {
-            this.markers = markers
-          console.log(this.markers);
-        }
-      );
-        
+            this.markers = markers          
+      }
+    );
   }
 
   clickedMarker(marker:marker){
@@ -32,25 +37,7 @@ export class AppComponent implements OnInit{
   //Start Position
   lat: number = 37.75134;
   lng: number = -122.39488;
-  
-  //Markers
-//   markers: marker[]= [
-//   {
-//     name:'Company 1',
-//     lat: 39.9000,
-//     lng: -86.1191
-//   },
-//   {
-//     name:'Company 2',
-//     lat: 39.9871,
-//     lng: -86.1233
-//   },
-//   {
-//     name:'Company 3',
-//     lat: 39.9987,
-//     lng: -86.1235
-//   }
-// ];
+
 
 ngOnInit() {
 
@@ -58,8 +45,8 @@ ngOnInit() {
    
   }
 
-
 }
+
 
 interface marker {
   id?: string;
