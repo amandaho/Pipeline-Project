23f9088043f0b8d356cb28292441779ad9b7675e
Christopher Woods <Christopher.Woods@LibertyMutual.com>
2017-07-25 15:34:37 -0400

    Messing with dashboard

--- Pipeline-Project/src/app/dashboard/dashboard.component.ts
+++ Pipeline-Project/src/app/dashboard/dashboard.component.ts
@@ -63,29 +63,5 @@ export class DashboardComponent implements OnInit {
     maintainAspectRatio: false
   };
 
-  type3 = 'bar';
-  data3 = {
-    labels: ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"],
-    datasets: [
-      {
-        label: "My First dataset",
-        data: [65, 59, 80, 81, 56, 55, 63, 22, 88, 45, 89, 99]
-      },
-      {
-        label: "My Second dataset",
-        data: [45, 34, 98, 56, 34, 23, 12, 22, 64, 23, 94, 34]
-      },
-      {
-        label: "My Third dataset",
-        data: [55, 25, 99, 67, 54, 66, 65, 34, 98, 65, 34, 78]
-      },
-      
-    ]
-  };
-  options3 = {
-    responsive: true,
-    maintainAspectRatio: false
-  };
-
 
 }
