fd165612842bfd9808ff69c16bb4f52423ca240f
Christopher Woods <Christopher.Woods@LibertyMutual.com>
2017-08-03 12:03:45 -0400

    Fix the refresh

--- server.js
+++ server.js
@@ -1,7 +1,14 @@
 const express = require('express');
 const app = express();
 app.use(express.static(__dirname + '/dist'));
+
+app.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + '/dist/index.html'));
+});
+
 app.listen(process.env.PORT || 8080);
+
+const path = require('path');
 
 const forceSSL = function() {
   return function (req, res, next) {
@@ -13,4 +20,5 @@ const forceSSL = function() {
     next();
   }
 }
+
 app.use(forceSSL());
